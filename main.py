c24f3e636981ad9b98556cb8b0152c3ee648e99d
Airi Kawahara <kwhrair928@gmail.com>
2018-03-30 03:50:24 +0000

    認証部分実装完了

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ JST = timezone(timedelta(hours=+9), 'JST')
 # ログイン画面
 @app.route('/')
 def top():
-  authentication_check()
-  return render_template('login.html', title='LOGIN', error=False)
+  if authentication_check():
+    return redirect('/schedule')
+  else:
+    return render_template('login.html', title='LOGIN', error=False)
 
-# ログインエラーの際は再度ログイン画面を表示する
+# ログイン処理
 @app.route('/login', methods=['POST'])
 def login():
   login_id = request.form['login_id']
@@ -26,13 +28,27 @@ def login():
   if login_check:
     return redirect('/schedule')
   else:
-    return render_template('login.html', title='LOGIN', error=True)
+    return redirect('/login')
+
+# ログインエラーの場合は再度ログイン画面を表示
+@app.route('/login')
+def login_failed():
+  return render_template('login.html', title='LOGIN', error=True)
+
+# ログアウト処理
+@app.route('/logout')
+def logout():
+  if 'login_token' in session:
+    session.pop('login_token', None)
+  return redirect('/')
 
 # スケジュール画面
 @app.route('/schedule')
 def schedule():
-  authentication_check()
-  return render_template('schedule.html', title='SCHEDULE')
+  if authentication_check():
+    return render_template('schedule.html', title='SCHEDULE')
+  else:
+    return redirect('/')
 
 # ユーザーの存在チェック
 def user_exists(login_id, password):
@@ -42,32 +58,74 @@ def user_exists(login_id, password):
   query = CLIENT.query(kind='users')
   query.add_filter('login_id', '=', login_id)
   query.add_filter('password', '=', hashed_password)
-  users = list(query.fetch())
+  try:
+    users = list(query.fetch())
+    if len(users) == 0:
+      return False
+    else:
+      # トークンを生成
+      letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+      token = ''.join([random.choice(letters) for _ in range(32)])
+      expiration_datetime = datetime.now(JST) + timedelta(days=+1)
 
-  if len(users) == 0:
-    return False
-  else:
-    # トークンを保存(有効期限は24時間)
-    letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    token = ''.join([random.choice(letters) for _ in range(32)])
-    expiration_datetime = datetime.now(JST) + timedelta(days=+1)
-    key = CLIENT.key('auth')
-    task = datastore.Entity(key)
-    task.update({
-      'login_id': login_id,
-      'token': token,
-      'expiration_datetime': expiration_datetime
-    })
-    CLIENT.put(task)
+      # トークンが既にDatastoreに登録されていないかチェック
+      delete_token()
+      query = CLIENT.query(kind='auth')
+      query.add_filter('token', '=', token)
+      results = list(query.fetch())
+      if len(results) != 0:
+        print("Token duplication error")
+        return False
 
-    # セッションにログイン情報を保存
-    
-    return True
+      # トークンを保存(有効期限は24時間)
+      key = CLIENT.key('auth')
+      task = datastore.Entity(key)
+      task.update({
+        'login_id': login_id,
+        'token': token,
+        'expiration_datetime': expiration_datetime
+      })
+      CLIENT.put(task)
+      # セッションにトークンを保存
+      session['login_token'] = token
+      return True
+  except:
+    print("Unexpected error:" + sys.exc_info()[0])
+    raise
 
 # 認証チェックを行う
 def authentication_check():
-  token = session.get('token')
-  print(token)
+  if not 'login_token' in session:
+    return False
+
+  # セッションに保存されているトークンが有効かどうか確認
+  delete_token()
+  login_token = session['login_token']
+  query = CLIENT.query(kind='auth')
+  query.add_filter('token', '=', login_token)
+  try:
+    results = list(query.fetch())
+    if len(results) == 0:
+      return False
+    else:
+      return True
+  except:
+    print("Unexpected error:" + sys.exc_info()[0])
+    raise
+
+# 有効期限切れのトークンをDatastoreから削除
+def delete_token():
+  now = datetime.now(JST)
+  query = CLIENT.query(kind='auth')
+  query.add_filter('expiration_datetime', '<', now)
+  try:
+    targets = list(query.fetch())
+    for target in targets:
+      key = target.__dict__['key']
+      CLIENT.delete(key)
+  except:
+    print("Unexpected error:" + sys.exc_info()[0])
+    raise
 
 if __name__ == '__main__':
   app.secret_key = 'some secret key'
